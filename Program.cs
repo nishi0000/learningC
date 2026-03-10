7876637aa3c95387de219ee99f83fb4e26e69eb1
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2026-03-10 10:26:02 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.ComponentModel;
+﻿
 
 
 string name = "Nishi";
 Console.WriteLine($"名前は{name}です");
 
-int count = 0;
 
-for (int i = 0; i < 10; i++)
+Console.WriteLine($"5 + 6 = {Add(5, 6)}");
+Greet(name);
+Console.WriteLine($"5 + 6 + 7 = {Add(5, 6, 7)}");
+
+int Add(int a, int b)
 {
-    count ++;
+    return a + b;
 }
 
-Console.WriteLine($"合計は{count}です。");
-
-for (int i = 1; i < 10; i++)
+int Add(int a, int b,int c)
 {
-    for (int j = 1; j < 10; j++)
-    {
-        // Console.WriteLine($"iは{i}です。");
-        // Console.WriteLine($"jは{j}です。");
-        Console.Write($"{i * j,3}");
-    }
-    Console.WriteLine();
-}
-
-string[] names = { "Alice", "Bob", "Charlie" };
-
-foreach (string name1 in names)
-{
-    Console.WriteLine($"{name1}さん、こんにちは！");
+    return a + b + c;
 }
 
 
-string? userString = "";
-
-while (userString != "exit")
+void Greet(string name)
 {
-   userString = Console.ReadLine();
+    Console.WriteLine($"こんにちは、{name}さん！");
 }
 
-for (int i = 0; i < 100; i++)
-{
-    if (i % 3 != 0 || i == 0)
-    {
-        continue;
-    }
-    Console.WriteLine(i);
-}
+
+
+
