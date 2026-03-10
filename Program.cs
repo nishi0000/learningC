32a0d874cb2f7dbe919490a007a6259aef8e8bf0
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2026-03-10 10:13:31 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -20,7 +20,31 @@ for (int i = 1; i < 10; i++)
     {
         // Console.WriteLine($"iは{i}です。");
         // Console.WriteLine($"jは{j}です。");
-        Console.Write(i * j + " ");
+        Console.Write($"{i * j,3}");
     }
     Console.WriteLine();
+}
+
+string[] names = { "Alice", "Bob", "Charlie" };
+
+foreach (string name1 in names)
+{
+    Console.WriteLine($"{name1}さん、こんにちは！");
+}
+
+
+string? userString = "";
+
+while (userString != "exit")
+{
+   userString = Console.ReadLine();
+}
+
+for (int i = 0; i < 100; i++)
+{
+    if (i % 3 != 0 || i == 0)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
 }
