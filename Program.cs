c52e0f39b0d1a247934a52d1c30dfac1374360b6
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2026-03-10 09:51:22 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,71 +5,22 @@ using System.ComponentModel;
 string name = "Nishi";
 Console.WriteLine($"名前は{name}です");
 
-Console.WriteLine($"あなたの年齢を入力してください。");
-var a = Console.ReadLine();
+int count = 0;
 
-if (!int.TryParse(a, out int age))
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("入力が無効です。");
-    return;
-}else if (age < 18)
-{
-    Console.WriteLine("未成年です。");
-}else if (age >= 18 && age < 65)
-{
-    Console.WriteLine("成人です。");
-}else
-{
-    Console.WriteLine("シニアです。");
+    count ++;
 }
 
-Console.WriteLine($"曜日を入力してください。");
-var b = Console.ReadLine();
+Console.WriteLine($"合計は{count}です。");
 
-switch (b)
+for (int i = 1; i < 10; i++)
 {
-    case "月":
-    case "火":
-    case "水":
-    case "木":
-    case "金":
-        Console.WriteLine("平日です。");
-        break;
-    case "土":
-    case "日":
-        Console.WriteLine("休日です。");
-        break;
-    case "水曜日":
-        Console.WriteLine("今日は水曜日です。");
-        break;
-    default:
-        Console.WriteLine("正しい曜日を入力してください。");
-        break;
+    for (int j = 1; j < 10; j++)
+    {
+        // Console.WriteLine($"iは{i}です。");
+        // Console.WriteLine($"jは{j}です。");
+        Console.Write(i * j + " ");
+    }
+    Console.WriteLine();
 }
-
-
-Console.WriteLine($"点数を入力してください。");
-var c = Console.ReadLine();
-
-if (!int.TryParse(c, out int score))
-{
-    Console.WriteLine("入力が無効です。");
-    return;
-}else if (score >= 90)
-{
-    Console.WriteLine("評価はAです。");
-}else if (score >= 80)
-{
-    Console.WriteLine("評価はBです。"); 
-}else if (score >= 70)
-{
-    Console.WriteLine("評価はCです。");
-}else if (score >= 60)
-{
-    Console.WriteLine("評価はDです。"); 
-
-}else
-{
-    Console.WriteLine("評価はFです。");
-}
-
